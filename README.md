e224b16a0f4ee4de29255eb68108b0310996c93a
myk245 <myk245@nyu.edu>
2020-10-07 19:19:01 -0400

    fetch requests with async await

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # EasyHTTP
 
-A simple custom HTTP library to handle asynchronous requests using Ajax callbacks.
+A simple custom HTTP library to handle asynchronous requests.
+
+### Versions
+
+Version 1: Ajax Callbacks
+Version 2: Promises
+Version 3: Async Await
---
+++ easy-http-3/app.js
@@ -0,0 +1,28 @@
+const http = new EasyHTTP;
+
+// Get Users
+// http.get('https://jsonplaceholder.typicode.com/users')
+//    .then(data => console.log(data))
+//    .catch(error => console.log(error))
+
+// User Data
+const data = {
+   name: 'Cindy Kei',
+   username: 'cindykei',
+   email: 'ck123@gmail.com'
+}
+
+// Create User
+// http.post('https://jsonplaceholder.typicode.com/users', data)
+//    .then(data => console.log(data))
+//    .catch(error => console.log(error))
+
+// Update User
+// http.put('https://jsonplaceholder.typicode.com/users/2', data)
+//    .then(data => console.log(data))
+//    .catch(error => console.log(error))
+
+// Delete User
+http.delete('https://jsonplaceholder.typicode.com/users/2')
+   .then(data => console.log(data))
+   .catch(error => console.log(error))
